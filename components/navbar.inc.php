e9c93d7e8e557418c9e80ba6eadf7012a74aa3da
kelvin <kelvinchengjiajun@gmail.com>
2022-02-28 21:18:15 +0800

    -Added bootstrap -Update Index page -Header and navigation components

---
+++ components/navbar.inc.php
@@ -0,0 +1,36 @@
+ <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img src="img/logo.png" alt="" /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link"  href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="bto.php">BTO</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="resale.php">Resale</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="faqs.php">FAQ</a>
+            </li>
+          </ul>
+            <>
+        </div>
+      </div>
+    </nav>
