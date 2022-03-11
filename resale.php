929036c7d504a66f153d4156ba181fe5c403fb3b
YoungDucky <quek.bevan@gmail.com>
2022-03-11 21:42:29 +0800

    I added resale.php, maybe I will look for the images later

---
+++ resale.php
@@ -0,0 +1,78 @@
+<?php
+require('components/head.inc.php');
+require('components/navbar.inc.php');
+?>
+
+<h1>Resale Flats</h1>
+
+	<ul class="image-list-small">
+    <li>
+	<p class="btoname"><strong>Serangoon(June 2025)</strong>
+            <a href="BTO1.php">
+      <img src="img/bto1.jpg">
+            </a>
+      <div class="details">
+        <p class="minsell">Minimum Selling Price:</p>
+        <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+    <li>
+	<p class="btoname"><strong>Jurong West(July 2023)</strong>
+    <img src="img/bto2.jpg"/>
+      <div class="details">
+        <p class="minsell">Minimum Selling Price:</p>
+        <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+    <li>
+	<p class="btoname"><strong>Yishun(August 2024)</strong>
+      <img src="img/bto3.jpg"/>
+      <div class="details">
+        <p class="minsell">Minimum Selling Price:</p>
+        <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+    <li>
+	<p class="btoname"><strong>Queenstown(January 2026)</strong>
+      <img src="img/bto4.jpg"/>
+      <div class="details">
+        <p class="minsell">Minimum Selling Price:</p>
+        <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+    <li>
+	<p class="btoname"><strong>Geylang(December 2027)</strong>
+     <img src="img/bto5.jpg"/>
+      <div class="details">
+        <p class="minsell">Minimum Selling Price:</p>
+        <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+    <li>
+	<p class="btoname"><strong>Bukit Merah(February 2024)</strong>
+      <img src="img/bto6.jpg"/>
+      <div class="details">
+      <p class="minsell">Minimum Selling Price:</p>
+      <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+	<li>
+	<p class="btoname"><strong>Toa Payoh(June 2025)</strong>
+      <img src="img/bto7.jpg"/>
+      <div class="details">
+      <p class="minsell">Minimum Selling Price:</p>
+      <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+	<li>
+	<p class="btoname"><strong>Hougang(March 2026)</strong>
+      <img src="img/bto8.jpg"/>
+      <div class="details">
+      <p class="minsell">Minimum Selling Price:</p>
+      <p class="maxsell">Maximum Selling Price:</p>
+      </div>
+    </li>
+</ul>
+</body>
+<script src="js/bootstrap.min.js"></script>	
+<?php require('components/footer.inc.php'); ?>
