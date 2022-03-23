fda44aa96751b4c62ac1203b25ee3ca45b39b33a
YoungDucky <quek.bevan@gmail.com>
2022-03-23 10:22:16 +0800

    Updated the resale images

--- resale.php
+++ resale.php
@@ -19,7 +19,7 @@ $select = mysqli_query($conn, "SELECT * FROM resale")
     {
         echo "<div class='col'>
     <div class='card h-100'>
-      <img src='".$data[5]."' class='card-img-top' alt=''>
+      <img src='".$data[9]."' class='card-img-top' alt=''>
       <div class='card-body'>
         <h5 class='card-title'>".$data[1]."</h5>
         <p class='card-text'>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
