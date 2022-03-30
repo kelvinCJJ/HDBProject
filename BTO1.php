0e6cc1bb0fa77d132867fc6efa0b8ee70e847e6c
FOO CHEE YONG <2002972@sit.singaporetech.edu.sg>
2022-03-31 04:14:50 +0800

    Stuck on $_get ~.~

--- BTO1.php
+++ BTO1.php
@@ -2,9 +2,11 @@
 require('components/head.inc.php');
 require('components/navbar.inc.php');
 require('config.php');
+
 ?>
 <!DOCTYPE html>
 <html>
+   
     <head>
         <script src="https://kit.fontawesome.com/dba75d4267.js" crossorigin="anonymous">
         </script>
@@ -14,66 +16,75 @@ require('config.php');
         </title>
     </head>
 <body>
-<div class ="housedetails">
+    
+
+        <div class ="housedetails">
     
     <div class ="housetitle">
         <h1>Fernvale</h1>
         <div class="row">
             <div>
-                <p>
-                    Location: Blk 432A Sengkang West Way (S)791432
-                </p>
+               
             </div>
                 
         </div>
     </div> 
     <div class ="bto_pic">
         
-       <div class="bto_pic1"><img src="img/bto/bto1_1.jpg" ></div>
-        <div><img src="img/bto/bto1_2.jpg" ></div>
-        <div><img src="img/bto/bto1_3.jpg" ></div>
-        <div><img src="img/bto/bto1_4.jpg" ></div>
-        <div><img src="img/bto/bto1_5.jpg" height="205" ></div>
-</div>
-    <div class="extra-info">
-        <h1> <i class="fa-solid fa-house-user"></i> Available Units : 200</h1>
-        <h2> <i class="fa-solid fa-bed"></i> Room Type: 3-Room, 4-Room</h2>
+         <?php
+    $btoid= $_GET["btoid"];
+            $select = mysqli_query($conn, "SELECT * FROM btoimages WHERE BTOId = btoid")
+            or die($conn->error);
+
+    if (mysqli_num_rows($select) > 0) {
+        $row = mysqli_fetch_all($select);
+    } else {
+        echo 'error';
+    }
+    foreach($row as $data)
+    {
+    echo"
+        <img src='".$data[1]."' ></div>
+        
+        ";
+    } ?>
+   
     </div>
-    <hr class="line">
-    <ul class="btm info">
-        <li>
-            <i class="fa-solid fa-person-walking"></i> 5mins(500m) walk to Seng Kang Mrt
             
-        </li>
-         <li>
-            <i class="fa-solid fa-school"></i> 1km distance to nearby schools such as Zhangde Pri
-            
-        </li>
-         <li>
-           <i class="fa-solid fa-store"></i> 2min walk to nearby supermarket
-            
-        </li>
+<?php      
+echo $_GET["btoid"];
+$select = mysqli_query($conn, "SELECT* FROM bto WHERE BTOId = btoid ")
+            or die($conn->error);
+
+    if (mysqli_num_rows($select) > 0) {
+        $row = mysqli_fetch_all($select);
+    } else {
+        echo 'error';
+    }
+?>
+<?php
+    foreach($row as $data){
+        
+    echo" <div class='extra-info'>
+        <h1> <i class='fa-solid fa-house-user'></i> Available Units :".$data[3]."</h1>
+    <h2> <i class='fa-solid fa-bed'></i> Room Type:".$data[4]."</h2>
+    </div>
+              
+    
+    <hr class='line'>
+   
         
         <h3>
-            Description:
-            *HDB 3,4 Room Apt*
-RARE 3,4A flat near Seng Kang MRT! 
-
-✔️ 3,4 Room 1000 sqft
-✔️ Mid-Floor Unit
-✔️ Corner Unit
-✔️ South West Facing
-✔️ Well-Maintained Unit
-✔️ Spacious and Efficient layout
-✔️Full privacy with only 4 units in a level. Point block!
-✔️ Extension needed
+            Description: ".$data[6]."
+            
         </h3> 
          
-    </ul>
-
-
-
-
+    
+}"
+    ;}
+    ?>
+    
+    
 </body>
 </html>
 
