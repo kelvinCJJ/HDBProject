b6b2692744bbad5f589d9b8de73f4a47125736d3
YoungDucky <quek.bevan@gmail.com>
2022-03-31 22:21:22 +0800

    Updated removed rogue }

--- resale.php
+++ resale.php
@@ -11,52 +11,36 @@ $select = mysqli_query($conn, "SELECT * FROM resale")
     } else {
         echo 'error';
     }
+    
     if (isset($_SESSION['userid']))
     {
      $userid = $_SESSION['userid'];
-     $selectu = mysqli_query($conn, "SELECT * FROM application WHERE userid = '$userid' AND resaleid = '1'")
+     $selectu = mysqli_query($conn, "SELECT * FROM application WHERE UserID = '$userid' AND ResaleID IS NOT NULL")
             or die($conn->error);
 
     if (mysqli_num_rows($selectu) > 0) {
         $message[] = 'applied';
     }
-    
-    if (isset($_POST['submit'])) {
-
-    $uid = mysqli_real_escape_string($conn, $userid);
-    $rid = mysqli_real_escape_string($conn, "1");
-    $date = mysqli_real_escape_string($conn, date("y/m/d"));
-    mysqli_query($conn, "INSERT INTO application(UserId,ResaleId,Date) VALUES('$uid','$rid',NOW())")
-            or die('query error');
-    $message[] = 'Your application was successful';
-    }
 }
 ?>
-<div class="container">
-    <form method="POST" class="needs-validation" novalidate="" autocomplete="off">
-                        <?php
-                        if (isset($message)) {
-                             foreach ($message as $message) {
-                                if (strpos($message, "succesful")) 
-                                    echo '<div class="alert alert-success" role="alert" onclick="this.remove()">' . $message . '</div>';
-                             }                           
-                        }
-                        ?>
-                        <input id="userid" type="text" class="form-control" name="userid" value="<?php $userid?>" hidden>
-                         <input id="resaleid" type="text" class="form-control" name="resaleid" value="1" hidden>
-                            <?php
-                            if(isset($message)){
-                                if (strpos($message, "applied")) {
-                                   echo "<button name='submit' type='submit' class='btn btn-primary ms-auto'>
-                                Apply Now </button>";
-                                } else {
-                                    echo "<button name='submit' type='submit' class='btn btn-secondary ms-auto' disabled>
-                                Applied </button>";
-                                }
-                            }
-                            ?>
-    </form>
+<body>
+    
+<form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+    <?php
+    if(isset($message))
+    {
+        if (strpos($message, "applied")) {
+         echo "<button name='submit' type='submit' class='btn btn-secondary ms-auto' disabled>
+        Applied </button>"; 
+        } else {
+             echo "<button name='submit' type='submit' class='btn btn-primary ms-auto'>Apply Now </button>";}
+    }
+    ?>
+</form>
+
+<div class="container">    
 <h1>Resale flats</h1>
+
 <div class="row row-cols-1 row-cols-md-3 g-4">
     <?php
     foreach($row as $data)
@@ -65,9 +49,7 @@ $select = mysqli_query($conn, "SELECT * FROM resale")
     <div class='card shadow-lg h-100'>
       <img src='".$data[9]."' class='card-img-top' alt=''>
       <div class='card-body'>
-        <h5 class='card-title'>".$data[1]." (".$data[4].")
-            
-                </h5>
+        <h5 class='card-title'>".$data[1]." (".$data[4].")</h5>
         <p class='card-text'>".$data[6].
             "<br>S$".$data[2].
             "<a href='/resaledetails.php?resaleid=".$data[0]."' class='btn btn-danger float-end'>Read more</a></p>
