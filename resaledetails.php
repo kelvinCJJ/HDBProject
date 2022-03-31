6fadcb02261e88734d7b5fea41b7a8780a9570e7
YoungDucky <quek.bevan@gmail.com>
2022-03-31 21:39:02 +0800

    Updated removed rogue }

--- resaledetails.php
+++ resaledetails.php
@@ -2,81 +2,93 @@
 require('components/head.inc.php');
 require('components/navbar.inc.php');
 require('config.php');
+
+if (isset($_SESSION['userid']))
+    {
+     $userid = $_SESSION['userid'];
+    }
+    $select = mysqli_query($conn, "SELECT * FROM application WHERE userid = '$userid' AND resaleid = '1'")
+            or die($conn->error);
+
+    if (mysqli_num_rows($select) > 0) {
+        $message[] = 'applied';
+    }
+    if (isset($_POST['submit'])) {
+
+    $uid = mysqli_real_escape_string($conn, $userid);
+    $rid = mysqli_real_escape_string($conn, "1");
+    $date = mysqli_real_escape_string($conn, date("d/m/y"));
+    mysqli_query($conn, "INSERT INTO application(UserId,ResaleId,Date) VALUES('$uid','$rid','$date')")
+            or die('query error');
+    $message[] = 'Your application was successful';
+    }
 ?>
+
+<?php
+if (isset($message)) {
+    foreach ($message as $message) {
+    if (strpos($message, "succesful")) 
+    echo '<div class="alert alert-success" role="alert" onclick="this.remove()">' . $message . '</div>';}}
+ ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <script src="https://kit.fontawesome.com/dba75d4267.js" crossorigin="anonymous">
-        </script>
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-        <title>
-            HDB Resale
-        </title>
-    </head>
+<?php
+    $resaleid = $_GET["resaleid"];
+
+$select1 = mysqli_query($conn, "SELECT * FROM resale WHERE ResaleId = $resaleid ")
+                or die($conn->error);
+
+        if (mysqli_num_rows($select1) > 0) {
+            $row1 = mysqli_fetch_all($select1);
+        } else {
+            echo 'error';
+        }
+        foreach ($row1 as $data1) {
+?>
 <body>
-<div class ="housedetails">
+    <div class="container">
     
-    <div class ="housetitle">
-        <h1>Fernvale</h1>
-        <div class="row">
-            <div>
-                <p>
-                    Location: Blk 432A Sengkang West Way (S)791432
-                </p>
+        <div class ="housedetails">
+
+        <div class ="housetitle">
+            <h1><?php echo "<h1>".$data1[1]."</h1>"?>
+            <div class="row">
             </div>
-                
+        </div> 
+        <div class ="row">
+            <div class="col-lg-4 col-md-4 col-xs-4 thumb">
+                <?php
+                $select = mysqli_query($conn, "SELECT * FROM resaleimages WHERE ResaleId = $resaleid")
+                        or die($conn->error);
+
+                if (mysqli_num_rows($select) > 0) {
+                    $row = mysqli_fetch_all($select);
+                } else {
+                    echo 'error';
+                }
+
+                foreach ($row as $data) {
+                    echo"<img src='" . $data[1] . "' >   ";
+                }?>
+            </div> 
+        </div>    
         </div>
-    </div> 
-    <div class ="bto_pic">
-        
-       <div class="bto_pic1"><img src="img/resale/resale1_1.jpg" ></div>
-        <div><img src="img/resale/resale1_2.jpg" ></div>
-        <div><img src="img/resale/resale1_3.jpg" ></div>
-        <div><img src="img/resale/resale1_4.jpg" ></div>
-        <div><img src="img/resale/resale1_5.jpg" ></div>
-</div>
-    <div class="extra-info">
-        <h1> <i class="fa-solid fa-house-user"></i> Available Units : 200</h1>
-        <h2> <i class="fa-solid fa-bed"></i> Room Type: 3-Room, 4-Room</h2>
-    </div>
-    <hr class="line">
-    <ul class="btm info">
-        <li>
-            <i class="fa-solid fa-person-walking"></i> 5mins(500m) walk to Seng Kang Mrt
-            
-        </li>
-         <li>
-            <i class="fa-solid fa-school"></i> 1km distance to nearby schools such as Zhangde Pri
-            
-        </li>
-         <li>
-           <i class="fa-solid fa-store"></i> 2min walk to nearby supermarket
-            
-        </li>
-        
+
+        <?php
+        echo" <div class='extra-info'>
+        <h1> <i class='fa-solid fa-house-user'></i> Address: " . $data1[6] . "</h1>
+        <h2> <i class='fa-solid fa-bed'></i> Room Type: " . $data1[4] . "</h2>
+        </div>
+        <hr class='line'>
         <h3>
-            Description:
-            *HDB 3,4 Room Apt*
-RARE 3,4A flat near Seng Kang MRT! 
-
-✔️ 3,4 Room 1000 sqft
-✔️ Mid-Floor Unit
-✔️ Corner Unit
-✔️ South West Facing
-✔️ Well-Maintained Unit
-✔️ Spacious and Efficient layout
-✔️Full privacy with only 4 units in a level. Point block!
-✔️ Extension needed
-        </h3> 
-         
-    </ul>
-
-
-
-
+            Description: " . $data1[11] . "
+        </h3>"
+            ;}
+        ?>
+    </div>
 </body>
-</html>
 
 <script src="js/bootstrap.min.js"></script>	
 <?php require('components/footer.inc.php'); ?>
+</html>
